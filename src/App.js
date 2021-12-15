8ace1dafc0c6ad148232467210e7b090c62c9e8d
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-15 13:05:59 -0400

    implement material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import {Link} from "react-router-dom";
+import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 function App() {
   return (
     <div>
-      <h1>Hola react routing</h1>
-        <nav>
-            <Link to="/users">Users</Link>
-            <Link to="/profiles">Profiles</Link>
-        </nav>
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" component="div">
+                    <Link to="/users">Users</Link>
+                </Typography>
+                <Typography variant="h6" color="inherit" component="div" pl="10">
+                    <Link to="/profiles">Profiles</Link>
+                </Typography>
+            </Toolbar>
+        </AppBar>
+
     </div>
   );
 }
