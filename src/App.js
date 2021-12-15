229a84974444a7dd70fc9687da1fecf85fd9a73b
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-15 12:58:37 -0400

    Add in project react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import {Link} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <h1>Hola react routing</h1>
+        <nav>
+            <Link to="/users">Users</Link>
+            <Link to="/profiles">Profiles</Link>
+        </nav>
     </div>
   );
 }
