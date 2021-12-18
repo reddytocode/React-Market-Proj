f5f72030948e46a0195676464aa6ee8e11b5e2c5
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-18 11:10:28 -0400

    comment code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Router} from "react-router-dom";
+import UsersPage from "./routes/users";
+import ProfilesPage from "./routes/profiles";
 
 
 const rootElement = document.getElementById('root')
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <App/>
+    // <BrowserRouter>
+    //     <Router>
+    //         <Route path="/"  component={<App/>}/>
+    //         <Route path="users" component={<UsersPage/>}/>
+    //         <Route path="profiles" component={<ProfilesPage/>}/>
+    //     </Router>
+    // </BrowserRouter>
     , rootElement
 );
