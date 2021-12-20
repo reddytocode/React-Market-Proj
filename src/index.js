976ba062a761ff65c42183582618479bcc5ce3c3
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-19 22:46:10 -0400

    add store

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import {BrowserRouter, Route, Router} from "react-router-dom";
-// import UsersPage from "./routes/users";
-// import ProfilesPage from "./routes/profiles";
+import {Provider} from "react-redux";
+import store from "./store";
 
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-    <App/>
-    // <BrowserRouter>
-    //     <Router>
-    //         <Route path="/"  component={<App/>}/>
-    //         <Route path="users" component={<UsersPage/>}/>
-    //         <Route path="profiles" component={<ProfilesPage/>}/>
-    //     </Router>
-    // </BrowserRouter>
+    <Provider store={store}>
+        <App/>
+    </Provider>
     , rootElement
 );
