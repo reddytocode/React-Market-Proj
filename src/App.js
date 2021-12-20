37942d518214c8887e20461c0b0bd4d2974a7922
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-20 08:23:34 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import {Link} from "react-router-dom";
 import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import { Routes, Route } from "react-router-dom";
+import UsersPage from "./routes/users";
+import ProfilesPage from "./routes/profiles";
 
 
 function App() {
   return (
-    <div>
-        <AppBar position="static">
-            <Toolbar variant="dense">
-                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" color="inherit" component="div">
-                    <Link to="/users">Users</Link>
-                </Typography>
-                <Typography variant="h6" color="inherit" component="div" pl="10">
-                    <Link to="/profiles">Profiles</Link>
-                </Typography>
-            </Toolbar>
-        </AppBar>
-
-    </div>
+      <div className="App">
+          <h1>Welcome to React Router!</h1>
+          <Routes>
+              <Route path="/" element={<UsersPage />} />
+              <Route path="about" element={<ProfilesPage />} />
+          </Routes>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Route, Router} from "react-router-dom";
+import {BrowserRouter, Route, Router, Switch} from "react-router-dom";
 import UsersPage from "./routes/users";
 import ProfilesPage from "./routes/profiles";
 
@@ -10,13 +10,8 @@ import ProfilesPage from "./routes/profiles";
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-    <App/>
-    // <BrowserRouter>
-    //     <Router>
-    //         <Route path="/"  component={<App/>}/>
-    //         <Route path="users" component={<UsersPage/>}/>
-    //         <Route path="profiles" component={<ProfilesPage/>}/>
-    //     </Router>
-    // </BrowserRouter>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
     , rootElement
 );
