09a1545cf062263e013cd3f705ab80f555d4c224
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-21 10:58:27 -0400

    add axios

--- src/components/ProductListComponent.js
+++ src/components/ProductListComponent.js
@@ -1,13 +1,28 @@
 import {Grid} from "@mui/material";
 import ProductComponent from "./ProductComponent";
+import {useEffect, useState} from "react";
+import axios from 'axios'
 
 const ProductListComponent = (props) =>{
-    const fake_product = {
-        image: "https://m.media-amazon.com/images/I/71E5zB1qbIL._FMwebp__.jpg",
-        name: "iPhone 13",
-        description: "Lorem ipsum ... "
-    }
-    const products = [fake_product, fake_product, fake_product, fake_product, fake_product, fake_product]
+    const defaultImage = "https://m.media-amazon.com/images/I/71E5zB1qbIL._FMwebp__.jpg"
+    const [products, setProducts] = useState([])
+
+
+    useEffect(() => {
+        const url = "https://martket-django-app.herokuapp.com/inventory/products/"
+
+        axios.get(url).then(response => {
+            const treatedProducts = response.data.map(item=>{
+                return {
+                    ...item,
+                    description: item.name,
+                    image: defaultImage
+                }
+            })
+            setProducts(treatedProducts)
+        })
+    }, [])
+
 
     const onAddProductToCart = props.onAddProductToCart
     const onRemoveProductFromCart = props.onRemoveProductFromCart
